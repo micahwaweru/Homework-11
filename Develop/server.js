0b8ef248621b44c7331274003f331060f77760f2
micahwaweru <micah.waweru@gmail.com>
2021-10-16 23:27:27 -0500

    commit 1

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+//Router here
+
+app.listen(PORT, function(){
+    console.log('Server listening PORT:' + PORT);
+})
